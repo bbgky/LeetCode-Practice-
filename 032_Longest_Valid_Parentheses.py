daf237c8b1a0bcdb6a028011f4721d7d04b5f486
bbgky <feihepku@gmail.com>
2016-06-06 19:36:31 -0700

    change spaces

--- 032_Longest_Valid_Parentheses.py
+++ 032_Longest_Valid_Parentheses.py
@@ -1,25 +1,25 @@
 class Solution(object):
     def longestValidParentheses(self,s):
-    """
-    :type s : str
-    :rtype :int
-    """
-    maxlen = 0
-    left = -1
-    stack = []
-    for i in range(len(s)):
-        if s[i]=='(':
-            stack.append(i)
-        else:
-            if stack==[]:
-                left=i
+        """
+        :type s : str
+        :rtype :int
+        """
+        maxlen = 0
+        left = -1
+        stack = []
+        for i in range(len(s)):
+            if s[i]=='(':
+                stack.append(i)
             else:
-                stack.pop()
                 if stack==[]:
-                    maxlen = max(maxlen,i-left)
+                    left=i
                 else:
-                    maxlen = max(maxlen,i-stack[-1])
-    return maxlen
+                    stack.pop()
+                    if stack==[]:
+                        maxlen = max(maxlen,i-left)
+                    else:
+                        maxlen = max(maxlen,i-stack[-1])
+        return maxlen
 
 
         
